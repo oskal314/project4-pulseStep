3b5e2740a13771d0707b3d1c34949d002ef18c02
oskal314 <artem-osipovartem3002@mail.ru>
2022-11-07 02:36:46 +0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -103,4 +103,13 @@ $(document).ready(function () {
     return false;
   });
 
+  // прокрутка якоря
+  $("body").on('click', '[href*="#"]', function (e) {
+    let fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+
+
+
 });
