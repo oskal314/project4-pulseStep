2a1b4071ce78d3882cc73683ce6a9fdfc830953a
oskal314 <artem-osipovartem3002@mail.ru>
2022-11-07 01:48:20 +0300

    update

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -12,9 +12,7 @@ $mail->CharSet = 'utf-8';
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'artemosipovartem12@gmail.com';                 // Наш логин
-$mail->Password = 'osipov2003';                           // Наш пароль от ящика
+$mail->SMTPAuth = true;                               // Enable SMTP authentication                         // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
@@ -40,5 +38,3 @@ if(!$mail->send()) {
 } else {
     return true;
 }
-
-?>
